518c8c68aff321f0585fe3af6f84a4903c21825d
Christophe Vila <kriss@vilanet.fr>
2025-08-31 12:55:07 +0000

    initial version of server component 1 (using mvn io.quarkus:quarkus-maven-plugin:create)

---
+++ server/component1/src/test/java/org/antiloop/myapp/server/component1/HealthResourceIT.java
@@ -0,0 +1,8 @@
+package org.antiloop.myapp.server.component1;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HealthResourceIT extends HealthResourceTest {
+    // Execute the same tests but in packaged mode.
+}
